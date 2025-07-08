e358e1dbc311121679b05e751d51dc7398619105
Stefan Karlsson <iam@stefan-karlsson.se>
2025-07-08 23:57:41 +0200

    refactor: update IAggregateRoot and IDomainEvent interfaces for improved clarity and consistency

--- src/Aviene.Domain.Abstractions/IAggregateRoot.cs
+++ src/Aviene.Domain.Abstractions/IAggregateRoot.cs
@@ -3,9 +3,9 @@ using JetBrains.Annotations;
 namespace Aviene.Domain.Abstractions;
 
 /// <summary>
-/// Marker interface to indicate that an entity is the aggregate root in the domain model.
-/// Aggregate roots are entry points to aggregate boundaries and are responsible for enforcing consistency within the aggregate.
+///   Marker interface to indicate that an entity is the aggregate root in the domain model.
+///   Aggregate roots are entry points to aggregate boundaries and are responsible for enforcing consistency within the
+///   aggregate.
 /// </summary>
 [PublicAPI]
-public interface IAggregateRoot;
-
+public interface IAggregateRoot : IHasDomainEvents;
--- src/Aviene.Domain.Abstractions/IDomainEvent.cs
+++ src/Aviene.Domain.Abstractions/IDomainEvent.cs
@@ -3,13 +3,13 @@ using JetBrains.Annotations;
 namespace Aviene.Domain.Abstractions;
 
 /// <summary>
-/// Represents a domain event that signals a significant change or occurrence within the domain model.
+///   Represents a domain event that signals a significant change or occurrence within the domain model.
 /// </summary>
 [PublicAPI]
 public interface IDomainEvent
 {
-    /// <summary>
-    /// Gets the timestamp indicating when the event occurred.
-    /// </summary>
-    DateTime OccurredAt { get; }
+  /// <summary>
+  ///   Gets the timestamp indicating when the event occurred.
+  /// </summary>
+  DateTimeOffset OccurredAt { get; }
 }
